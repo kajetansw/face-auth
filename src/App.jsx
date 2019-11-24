ac564787a007fe545219a60d0092d944144c8590
Kajetan Swiatek <kajetan.swiatek@gmail.com>
2019-11-24 11:08:48 +0100

    added react-router-dom * added Home, Login and Register pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
+import Register from './Register/Register';
+import Login from './Login/Login';
+import Home from './Home/Home';
+
 import './App.css';
 
 function App() {
   return (
-    <h1>Example</h1>
+    <BrowserRouter>
+      <nav>
+        <Link to="/register">Register</Link>
+        <Link to="/login">Login</Link>
+      </nav>
+
+      <Switch>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch> 
+    </BrowserRouter>
   );
 }
 
