a41d9fa775784d85116c68aa1282fd23a971e767
Kajetan Swiatek <kajetan.swiatek@gmail.com>
2019-11-24 14:08:41 +0100

    added SampleFaceImages component

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
+
 import Register from './Register/Register';
 import Login from './Login/Login';
 import Home from './Home/Home';
+import SampleFaceImages from './SampleFaceImages/SampleFaceImages';
 
 import './App.css';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <nav>
-        <Link to="/register">Register</Link>
-        <Link to="/login">Login</Link>
-      </nav>
+export default class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <nav>
+          <Link to="/register">Register</Link>
+          <Link to="/login">Login</Link>
+        </nav>
 
-      <Switch>
-        <Route path="/register">
-          <Register />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch> 
-    </BrowserRouter>
-  );
+        <SampleFaceImages />
+
+        <Switch>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
-
-export default App;
---
+++ src/SampleFaceImages/SampleFaceImages.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+import './SampleFaceImages.css';
+
+export default class SampleFaceImages extends Component {
+  render() {
+    return <div className="SampleFaceImages__container"></div>
+  }
+}
