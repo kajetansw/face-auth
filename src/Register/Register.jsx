2586d8df1fc6580849e084521ad84ae9f513d90d
Kajetan Swiatek <kajetan.swiatek@gmail.com>
2019-11-24 11:32:38 +0100

    initializing and stopping web cam on Register page

--- src/Register/Register.jsx
+++ src/Register/Register.jsx
@@ -1,9 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component, createRef } from 'react';
 
 export default class Register extends Component {
+  videoRef;
+
+  constructor(props) {
+    super(props);
+    this.videoRef = createRef();
+  }
+
+  componentDidMount() {
+    this.initializeWebCam();
+  }
+
+  componentWillUnmount() {
+    this.stopWebCam();
+  }
+
+  initializeWebCam = () => {
+    if (navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true })
+        .then(stream => this.videoRef.current.srcObject = stream)
+        .catch(console.error);
+    }
+  }
+
+  stopWebCam = () => {
+    const stream = this.videoRef.current.srcObject;
+    const tracks = stream.getTracks();
+
+    tracks.forEach(track => track.stop());
+
+    this.videoRef.current.srcObject = null;
+  }
+
   render() {
     return (
-      <h1>Register page</h1>
+      <React.Fragment>
+        <video autoPlay="true" id="webcam" ref={this.videoRef}></video>
+      </React.Fragment>
     );
   }
 }
