0e45cfd70533dfdef0b177b9e3b96815e91dc6c1
Kajetan Swiatek <kajetan.swiatek@gmail.com>
2019-11-24 14:32:32 +0100

    added taking snapshots of webcam to Register component

--- src/Register/Register.jsx
+++ src/Register/Register.jsx
@@ -1,11 +1,19 @@
 import React, { Component, createRef } from 'react';
 
+import './Register.css';
+
 export default class Register extends Component {
+  state = {
+    faceImages: []
+  };
+
   videoRef;
+  canvasRef;
 
   constructor(props) {
     super(props);
     this.videoRef = createRef();
+    this.canvasRef = createRef();
   }
 
   componentDidMount() {
@@ -33,10 +41,33 @@ export default class Register extends Component {
     this.videoRef.current.srcObject = null;
   }
 
+  takeSnapshot = () => {
+    const canvasEl = this.canvasRef.current;
+    const canvasContext = canvasEl.getContext('2d');
+    canvasContext.drawImage(this.videoRef.current, 0.0, 0.0, canvasEl.width, canvasEl.height);
+    this.setState((prevState, props) => {
+      return { 
+        faceImages: [...prevState.faceImages, canvasEl.toDataURL('image/jpg')] 
+      };
+    });
+  }
+
   render() {
     return (
       <React.Fragment>
-        <video autoPlay="true" id="webcam" ref={this.videoRef}></video>
+        <video 
+          autoPlay={true} 
+          id="webcam" 
+          ref={this.videoRef} 
+          onClick={this.takeSnapshot}
+        ></video>
+        <canvas 
+          className="Register__capturing-canvas" 
+          ref={this.canvasRef}
+          width="500" 
+          height="350"
+        ></canvas>
+        {this.state.faceImages.map((image, idx) => (<img src={image} alt={`Face sample ${idx}`}/>))}
       </React.Fragment>
     );
   }
